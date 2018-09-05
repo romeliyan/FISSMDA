7307f64876e4df923296044b2fa77ea5717b4fbf
M4C3RAVO <chamika.dimantha@gmail.com>
2018-09-05 11:13:39 +0530

    Vehicle Sub System

--- src/fissmda/DBConnection.java
+++ src/fissmda/DBConnection.java
@@ -16,11 +16,11 @@ import java.sql.SQLException;
  */
 public class DBConnection {
     
-    public Connection getConnection() throws ClassNotFoundException, SQLException
+    public static Connection getConnection() throws ClassNotFoundException, SQLException
     {
         try
 	{
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/fissmda", "root", "");
             return connection;
 	}
